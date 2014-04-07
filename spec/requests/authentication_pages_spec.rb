ed7d67b492a8f643ad5df5d00f0787d0664ecd67
ZHAO Liyao <zly0009@gmail.com>
2014-04-07 15:44:19 -0500

    change authentication test to make sure signout, profile, settings and users links wont appear when a user isnt signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,10 +15,15 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
+    let(:user) { FactoryGirl.create(:user) }
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Users', href: users_path) }
+      it { should_not have_link('Profile', href: user_path(user)) }
+      it { should_not have_link('Settings', href: edit_user_path(user)) }
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_error_message('Invalid')}
